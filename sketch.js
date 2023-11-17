f34bcb6b0ce403fb3e03e9a4dd7972530c43017d
mraveragedot <nohlgustafson05@gmail.com>
2023-11-17 12:16:55 -0600

    started moving character around

--- sketch.js
+++ sketch.js
@@ -5,6 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let farmer;
+
+function preload(){
+  farmer = loadImage("Farmer.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,5 +17,40 @@ function setup() {
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 100);
+  Player.display();
 }
+
+class Player{
+  constructor(x,y,theImage){
+    this.x = x;
+    this.y = y;
+    this.dx = 5;
+    this.dy = 5;
+
+    this.myImage = theImage;
+  }
+
+  display(){
+    image(this.myImage,this.x, this.y);
+  }
+
+  moveCharacter(){
+    if (keyIsDown(65)) {
+      this.x -= 5;
+    }
+  
+    if (keyIsDown(68)) {
+      this.x += 5;
+    }
+  
+    if (keyIsDown(87)) {
+      this.y -= 5;
+    }
+  
+    if (keyIsDown(83)) {
+      this.y += 5;
+    }
+
+  }
+}
+
