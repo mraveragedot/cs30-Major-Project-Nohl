3f9cc199c6fc271b2930f6bc52c785c1b4cbc421
mraveragedot <nohlgustafson05@gmail.com>
2023-11-21 12:11:43 -0600

    made character able to flip when walking

--- sketch.js
+++ sketch.js
@@ -5,19 +5,26 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let farmer;
+let direction = [1,1];
+let player, merchant, farmer;
 
 function preload(){
   farmer = loadImage("Farmer.png");
+  merchant = loadImage("merchant.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  player = new Player(width/2, height/2, farmer);
 }
 
 function draw() {
   background(220);
-  Player.display();
+  backdrop();
+  player.display();
+  player.moveCharacter();
+  rect(100,100,100,100);
+  
 }
 
 class Player{
@@ -31,26 +38,47 @@ class Player{
   }
 
   display(){
-    image(this.myImage,this.x, this.y);
+    if(direction[0] === 1){
+      imageMode(CENTER);
+      image(this.myImage,this.x, this.y, 90, 90);
+    }
+    else{
+      push();
+      imageMode(CENTER);
+      scale(direction);
+      image(this.myImage, -this.x, this.y, 90, 90);
+      pop();
+    }
+    imageMode(CORNER);
   }
 
   moveCharacter(){
     if (keyIsDown(65)) {
-      this.x -= 5;
+      this.x -= this.dx;
+      direction = [-1,1];
     }
   
     if (keyIsDown(68)) {
-      this.x += 5;
+      this.x += this.dx;
+      direction = [1,1];
+
     }
   
     if (keyIsDown(87)) {
-      this.y -= 5;
+      this.y -= this.dy;
+
     }
   
     if (keyIsDown(83)) {
-      this.y += 5;
+      this.y += this.dy;
+
     }
 
   }
 }
 
+function backdrop(){
+  imageMode(TOP);
+  image(merchant, width / 16*14,0, 100,100);
+}
+ 
